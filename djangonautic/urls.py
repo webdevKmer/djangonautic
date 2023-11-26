c51b8dcb123eb9bb080c31b8327df76ee3df52c5
webdevKmer <webdevKmer@gmail.com>
2023-11-26 17:18:55 +0000

    create the project

---
+++ djangonautic/urls.py
@@ -0,0 +1,16 @@
+"""
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', views.about),
+    path('', views.index, name='home'),
+    path('blog/', include('blog.urls')),
+]
